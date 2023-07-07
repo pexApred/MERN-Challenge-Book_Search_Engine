aa01bbda0773269201a36da68ac2e7aad0d9a9f6
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-07 11:26:35 -0400

    Created mutations.js file in utils based on schemas for querys

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+// SaveBookInput may need to just be BookInput...
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: SaveBookInput!) {
+        saveBook(input: $input) {
+            _id
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`;
