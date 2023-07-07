57a297d98f29ba03f76a308b02e4cccfa2fb6f12
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-07 10:42:33 -0400

    Translated User.js and bookschema into a queries.js file in utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
