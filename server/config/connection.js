1654a306b544128c4bcd2f5feebbd4a83aeb1fea
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-14 16:47:48 -0400

    changed database name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernbooks');
 
 module.exports = mongoose.connection;
